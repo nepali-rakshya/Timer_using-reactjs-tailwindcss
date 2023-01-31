120cb291e34db7912dd63f5477f7de0bff089847
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-31 14:27:18 +0545

    commit

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { useState } from "react";
 
 const Content = () => {
+  const [sec, setSec] = useState(0);
+  const [min, setMin] = useState(0);
+  const [hour, setHour] = useState(0);
+  const [day, setDay] = useState(0);
+
+  const handleClick = (e) => {
+    console.log("button" + e.target);
+  };
+
   return (
     <div className="flex justify-center items-center h-screen font-body tracking-wider">
       <div className="bg-sky-500 w-[35vw] rounded-lg pt-8 px-8 relative ">
@@ -15,15 +25,59 @@ const Content = () => {
         </div>
 
         <div className="relative w-[32rem] h-36 bg-white rounded-xl left-[50%] -translate-x-[50%] px-12 py-8">
-          <div className="flex text-5xl">
-            <p>2</p>
-            <p>13</p>
-            <p>35</p>
-            <p>15</p>
+          <div className="flex text-5xl gap-16 w-[100%]">
+            <div className="flex flex-col items-center gap-1">
+              <p>{day}</p>
+              <p className="text-gray-400 font-medium opacity-75 text-sm">
+                DAYS
+              </p>
+            </div>
+            <div className="flex flex-col items-center gap-1">
+              <p>{hour}</p>
+              <p className="text-gray-400 font-medium opacity-75 text-sm">
+                HOURS
+              </p>
+            </div>
+            <div className="flex flex-col items-center gap-1">
+              <p>{min}</p>
+              <p className="text-gray-400 font-medium opacity-75 text-sm">
+                MINUTES
+              </p>
+            </div>
+            <div className="flex flex-col items-center gap-1">
+              <p>{sec}</p>
+              <p className="text-gray-400 font-medium opacity-75 text-sm">
+                SECONDS
+              </p>
+            </div>
           </div>
-          <div className="absolute left-[50%] -translate-x-[50%] top-[7.4rem]">
-            <button className="bg-white rounded-full ">
-              <img src="/img/play.png" alt="play-button" />
+
+          <div className="absolute left-[50%] -translate-x-[50%] top-[7rem] ">
+            <button
+              className="w-16 h-16 hover:opacity-95"
+              onClick={handleClick}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#028EC7"
+                stroke="#028EC7"
+                stroke-width="0.6"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0z"
+                />
+
+                <path
+                  stroke-linecap="round"
+                  stroke="#fff"
+                  stroke-linejoin="round"
+                  d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
+                />
+              </svg>
             </button>
           </div>
         </div>
